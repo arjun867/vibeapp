c625f2f4f6098b3878af36e7a99270b6c4c616ba
arjun867 <sammengiarjun@gmail.com>
2024-05-21 10:11:00 +0530

    adding all model in admin page

--- vibeapp/admin.py
+++ vibeapp/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import CustomUser
+from .models import CustomUser,BlockedUser,Message,VibeMatch
 
 admin.site.register(CustomUser)
+admin.site.register(Message)
+admin.site.register(VibeMatch)
+admin.site.register(BlockedUser)
